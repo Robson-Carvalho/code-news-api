b9b7b207385f8964c1a0bfb96cd550e7bc7a5e53
Adriano Croco <adriano.daniel@soudaki.com>
2023-07-11 20:45:45 -0300

    feature: adicao de sqlite

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const NewsAPI = require("newsapi");
 const express = require("express");
 const bodyParser = require("body-parser");
+const sqlite3 = require('sqlite3').verbose();
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3030;
 const newsapi = new NewsAPI(`${process.env.SECRET_API}`);
+const db = new sqlite3.Database('./data/database.db');
+
+db.run('PRAGMA foreign_keys=ON');
+db.run('PRAGMA journal_mode=WAL');
+db.run('CREATE TABLE IF NOT EXISTS news (data JSON)');
+
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,6 +24,9 @@ app.use((req, res, next) => {
 app.disable("x-powered-by");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 
 const news = {
   requisitionDate: "",
@@ -42,15 +52,33 @@ app.get("/", async (req, res) => {
       })
       .then((response) => {
         news.requisitionDate = date(1);
-        news.result = [];
         news.result = response.articles;
+        const document = response.articles;
+        db.run('INSERT INTO news (data) VALUES (?)', [JSON.stringify(document)], function (err) {
+          if (err) {
+            return console.error(err.message);
+          }
+          console.log('A new document has been inserted with ID', this.lastID);
+        });
       });
 
     return res.json({ news });
   }
 
-  return res.json({ news });    
+  return res.json({ news });
 });
+
+app.get("/news", async (req, res) => {
+  db.all('SELECT data FROM news', function (err, rows) {
+    if (err) {
+      return console.error(err.message);
+    }
+    return res.json({ rows });
+  });
+})
+
+
+
 
 app.listen(port, () =>
   console.log(`server running in http://localhost:${port}`)
